a85c1ccc722ff22cfdd0fa8dde74fa01470e63ba
Mateusz Byczkowski <byczkowski38@gmail.com>
2017-04-06 08:53:33 +0200

    initial commit

---
+++ Client/Client.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        public string Serwer { get; private set; }
+        public int Port { get; private set; }
+        public TcpClient Tcp { get; private set; }
+        public Client(string server, int port)
+        {
+            Serwer = server;
+            Port = port;
+        }
+        public bool ConnectWithServer(string IP)
+        {
+            try
+            {
+                Tcp = new TcpClient(Serwer, Port);
+                if (Tcp.Connected)
+                {
+                    SendCommunique(IP);
+                    Console.WriteLine("Polaczono z centrala");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Nie uzyskano polaczenia z centrala");
+                    return false;
+                }
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e);
+            }
+            return false;
+        }
+        
+        public string GetCommunique()
+        {
+            BinaryReader reader = new BinaryReader(Tcp.GetStream());
+            byte message = reader.ReadByte();
+            string data = Convert.ToString(message, 2);
+            return data.PadLeft(8, '0');
+        }
+        public void SendCommunique(string communique)
+        {
+            try
+            {
+                BinaryWriter writer = new BinaryWriter(Tcp.GetStream());
+                // Byte[] data = Encoding.ASCII.GetBytes(communique);
+                //System.Console.WriteLine("SendCommunique " + communique);
+                writer.Write(Convert.ToString(communique));
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e);
+            }
+        }
+
+        public string GetIP()
+        {
+            string strHostName = "";
+            strHostName = System.Net.Dns.GetHostName();
+
+            IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(strHostName);
+
+            IPAddress[] addr = ipEntry.AddressList;
+
+            return addr[addr.Length - 1].ToString();
+        }
+    }
+}
+    
---
+++ Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        public static string output;
+        static private void tcpdump()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "C:\\users\\mateu\\downloads\\WinDump.exe";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+
+            // Synchronously read the standard output of the spawned process. 
+            output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+        }
+
+        private static Client client;
+        private static string MyIP;
+
+        static void Main(string[] args)
+        {
+            string ServerIP = "127.0.0.1";
+            Console.WriteLine("Podaj IP urządzenia");
+            string MyIP = Console.ReadLine();
+            client = new Client(ServerIP, 1000);
+
+            Thread thr = new Thread(tcpdump);
+            thr.Start();
+
+            if (client.ConnectWithServer(MyIP))
+            {
+                Console.WriteLine("Polaczono z serwerem");
+                while (true)
+                {
+                    //Console.WriteLine("Komunikat do wyslania: docelowo TCPDump");
+                    //string comumunique = Console.ReadLine();
+                    //client.SendCommunique(comumunique);
+                    client.SendCommunique(output);
+                }
+            }
+            Console.WriteLine("Konczenie pracy");
+            //Local IP
+            //Console.WriteLine(client.GetIP().ToString());
+            Console.ReadLine();
+        }
+    }
+}
